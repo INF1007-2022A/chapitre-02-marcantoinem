341fef0ca4121cdb9abd138bb922f080de65edbd
lemark <marcantoine_manningham.com>
2022-09-05 01:42:36 -0400

    Résolution de l'exercice

--- exercice.py
+++ exercice.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
+    nouveau_mot = ''
+    for lettre in mot:
+        lettre_ascii = ord(lettre)
+        if 97 <= lettre_ascii <= 122 or 224 <= lettre_ascii <= 253:
+            nouveau_mot += chr(lettre_ascii - 32)
+        else:
+            nouveau_mot += lettre
+    return nouveau_mot
 
 
 if __name__ == '__main__':
